7670dbf436ca36fab0f052e247809e6187889159
Kier Borromeo <seraphipod@gmail.com>
2015-01-25 20:51:14 +0800

    Create First.jsx

---
+++ src/components/First.jsx
@@ -0,0 +1,14 @@
+/** @jsx React.DOM */
+var React = require('react');
+
+var First = React.createClass({
+  render: function () {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+});
+
+module.exports = First;
