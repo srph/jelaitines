7085f955552e68f07cee150f97197db3b392ad3f
Kier Borromeo <seraphipod@gmail.com>
2015-01-26 18:50:18 +0800

    Create Ikalawa.jsx

---
+++ src/components/Ikalawa.jsx
@@ -0,0 +1,16 @@
+/** @jsx React.DOM */
+var React = require('react');
+
+var Ikalawa = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h3> All The </h3>
+        <h1> Sweet </h1>
+        <h2> Memories </h2>
+      </div>
+    );
+  }
+});
+
+module.exports = Ikalawa;
