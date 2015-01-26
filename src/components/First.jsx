21370734dc35d95735cae5a8a08e7388f62bbade
Kier Borromeo <seraphipod@gmail.com>
2015-01-26 19:09:43 +0800

    Added static content

--- src/components/First.jsx
+++ src/components/First.jsx
@@ -5,7 +5,10 @@ var First = React.createClass({
   render: function () {
     return (
       <div>
-        
+        <h2> You Are The </h2>
+        <h1> Best Person </h1>
+        <h2> To Be In </h2>
+        <h1> My Life </h1>
       </div>
     );
   }
