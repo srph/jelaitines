be04b12a2df9e71973c24b1c3f7fdcdf355f94e5
srph <seraphipod@gmail.com>
2015-02-02 01:56:07 +0800

    Added set images.

--- src/components/Carousel/Gallery.jsx
+++ src/components/Carousel/Gallery.jsx
@@ -24,35 +24,35 @@ var Gallery = React.createClass({
     return (
       <div className="clearfix" style={ContainerStyle}>
         <div style={CellStyle}>
-          <Thumbnail src="dist/img/k.jpg" size="1" />
+          <Thumbnail src="dist/img/set-1/1.jpg" size="1" />
         </div>
 
         <div style={CellStyle}>
-          <Thumbnail src="dist/img/k.jpg" size="2v" />
-          <Thumbnail src="dist/img/k.jpg" size="2v" />
+          <Thumbnail src="dist/img/set-2/1.jpg" size="2v" />
+          <Thumbnail src="dist/img/set-2/2.jpg" size="2v" />
         </div>
 
         <div style={CellStyle}>
           <div style={{ 'float': 'left', 'width': (BASE_SIZE / 2) }}>
-            <Thumbnail src="dist/img/k.jpg" size="3" />
-            <Thumbnail src="dist/img/k.jpg" size="3" />
+            <Thumbnail src="dist/img/set-3/2.jpg" size="3" />
+            <Thumbnail src="dist/img/set-3/3.jpg" size="3" />
           </div>
 
-          <Thumbnail src="dist/img/k.jpg" size="2h" />
+          <Thumbnail src="dist/img/set-3/1.jpg" size="2h" />
         </div>
 
         <div style={CellStyle}>
-          <Thumbnail src="dist/img/k.jpg" size="3" />
-          <Thumbnail src="dist/img/k.jpg" size="3" />
+          <Thumbnail src="dist/img/set-4/2.jpg" size="3" />
+          <Thumbnail src="dist/img/set-4/1.jpg" size="3" />
 
-          <Thumbnail src="dist/img/k.jpg" size="4" />
-          <Thumbnail src="dist/img/k.jpg" size="4" />
-          <Thumbnail src="dist/img/k.jpg" size="4" />
-          <Thumbnail src="dist/img/k.jpg" size="4" />
-          <Thumbnail src="dist/img/k.jpg" size="4" />
-          <Thumbnail src="dist/img/k.jpg" size="4" />
-          <Thumbnail src="dist/img/k.jpg" size="4" />
-          <Thumbnail src="dist/img/k.jpg" size="4" />
+          <Thumbnail src="dist/img/set-4/3.jpg" size="4" />
+          <Thumbnail src="dist/img/set-4/4.jpg" size="4" />
+          <Thumbnail src="dist/img/set-4/5.jpg" size="4" />
+          <Thumbnail src="dist/img/set-4/6.jpg" size="4" />
+          <Thumbnail src="dist/img/set-4/7.jpg" size="4" />
+          <Thumbnail src="dist/img/set-4/8.jpg" size="4" />
+          <Thumbnail src="dist/img/set-4/9.jpg" size="4" />
+          <Thumbnail src="dist/img/set-4/10.jpg" size="4" />
         </div>
       </div>
     );
