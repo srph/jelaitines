1655401eba408a1f1eb36f71a11285ee36e0dce6
srph <seraphipod@gmail.com>
2015-02-01 20:20:16 +0800

    Fixed thumbnail path

--- src/components/Carousel/Gallery.jsx
+++ src/components/Carousel/Gallery.jsx
@@ -1,7 +1,7 @@
 /** @jsx React.DOM */
 var React = require('react');
 
-var Thumbnail = require('./Thumbnail');
+var Thumbnail = require('../Thumbnail');
 
 var Gallery = React.createClass({
   render: function () {
